754f09caef200b2ea564859ead257e179bdccfea
KC0110 <kshitij.choubey.1331@gmail.com>
2022-09-27 11:05:30 +0530

    gitignore added

--- netscript.sh
+++ netscript.sh
@@ -1,30 +1,87 @@
+value(){
+
+echo  -e "Enter the value : \c  "
+}
+first_page(){
+echo "*******************************************************************************************"
 echo "[1] Edit a connection"
 echo "[2] Activate a connection"
 echo "[3] set hostname"
-echo " " 
-echo  -e "Enter the value : \c " 
+echo " "
+value
 read value1
+}
 
-if [ $value1 == 1 ]
-then
+edit_a_connection(){
+
+echo "*******************************************************************************************"
 	echo "[1] Add"
 	echo "[2] Edit"
 	echo "[3] Delete"
-        echo  -e "Enter the value : \c " 
+	echo " "
+	value
 	read value2
-	if [ $value2 == 1 ]
-	then
-		echo "select type"
+}
+add_connection(){
+	
+echo "*******************************************************************************************"
+		echo "select connection type : "
 		echo "[1] Ethernet"
 		echo "[2] wifi"
-                echo  -e "Enter the value : \c " 
-	        read value3
-		
+	        echo " "
+		value
+		read value3
+	}
+
+ethernet(){
+
+<<comment
+	configuration{
+
+		     echo " Select configuration :"
+		     echo "[1] Automatic"
+		     echo "[2] Manual"
+		     echo " "
+		     value
+		     read value4
+	     }
+	command1(){
+		nmcli c add conname $con_name type $value3 ifname $dev_name
+	}
+	command2(){
+		nmcli c mod $con_name ipv4.addresses $ipadd;
+		nmcli c mod $con_name ipv4.gateway $gate;
+		nmcli c mod $con_name ipv4.dns $dns;
+		nmcli c mod $con_name ipv4.method manual;
+	}
+
+		     echo -e "Connection name : \c"
+		     read con_name
+		     echo -e "device name : \c"
+		     read dev_name
+		     configuration
+		     if [ $value == 1 ]
+		     then
+			     command1
+		     if [ $value4 == 2 ]
+		     then
+			     command1
+			     command2
+		     else
+			     configuration
+		      fi
+comment
+	      }
+first_page
+if [ $value1 == 1 ]
+then
+	edit_a_connection
+	if [ $value2 == 1 ]
+	then
+		add_connection
 	        if [ $value3 == 1 ]
 	        then
-		     echo "Conn  name :"
-		     echo "device name : "
-		     echo "ip add :"
+		    ethernet
 		fi
                     
 	             
